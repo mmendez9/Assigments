c3f3ca5a44f06961e7a491c1973262bee68cb691
mmendez9 <hikari1995>
2016-04-05 14:22:30 -0400

    Radio buttons added

--- src/p2/TraficLightTimerDemo.java
+++ src/p2/TraficLightTimerDemo.java
@@ -5,5 +5,70 @@ package p2;
  */
 
 
-public class TraficLightTimerDemo {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+
+public class TraficLightTimerDemo extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Initialize three circles in the array
+        Circle[] circle = new Circle[3];
+
+        circle[0] = new Circle();
+        circle[0].setRadius(50);
+        circle[0].setStroke(Color.BLACK);
+        circle[0].setFill(Color.RED);
+
+        circle[1] = new Circle();
+        circle[1].setRadius(50);
+        circle[1].setStroke(Color.BLACK);
+        circle[1].setFill(Color.YELLOW);
+
+        circle[2] = new Circle();
+        circle[2].setRadius(50);
+        circle[2].setStroke(Color.BLACK);
+        circle[2].setFill(Color.GREEN);
+
+        // Create a VBox pane for the circles
+        VBox vBox = new VBox(20);
+        vBox.setStyle("-fx-background-color: grey");
+        vBox.setPadding(new Insets(20));
+        vBox.getChildren().addAll(circle[0], circle[1], circle[2]);
+
+        // Create a pane for the vBox
+        GridPane pane = new GridPane();
+        pane.setPrefHeight(300);
+        pane.setPrefWidth(100);
+        pane.setPadding(new Insets(10, 10, 10, 10));
+        pane.setAlignment(Pos.CENTER);
+        pane.getChildren().addAll(vBox);
+
+        // Create the scene for the panes
+        Scene scene = new Scene(pane, 250, 450);
+        primaryStage.setTitle("Traffic Light");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        EventHandler<ActionEvent> eventHandler = e -> {
+            if (circle[0].getFill() == Color.RED) {
+                circle[1].setFill(Color.WHITE);
+                circle[2].setFill(Color.WHITE);
+            }
+        };
+    }
 }
