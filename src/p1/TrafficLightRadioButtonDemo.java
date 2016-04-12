71ed21584a176666ea35e2a974a6790626642f60
Mayra Mendez <mmendez9@dtcc.edu>
2016-04-11 17:41:37 -0700

    More demos added

--- src/p1/TrafficLightRadioButtonDemo.java
+++ src/p1/TrafficLightRadioButtonDemo.java
@@ -29,24 +29,24 @@ public class TrafficLightRadioButtonDemo extends Application{
         Circle[] circle = new Circle[3];
 
         circle[0] = new Circle();
-        circle[0].setRadius(50);
+        circle[0].setRadius(25);
         circle[0].setStroke(Color.BLACK);
         circle[0].setFill(Color.WHITE);
 
         circle[1] = new Circle();
-        circle[1].setRadius(50);
+        circle[1].setRadius(25);
         circle[1].setStroke(Color.BLACK);
         circle[1].setFill(Color.WHITE);
 
         circle[2] = new Circle();
-        circle[2].setRadius(50);
+        circle[2].setRadius(25);
         circle[2].setStroke(Color.BLACK);
         circle[2].setFill(Color.WHITE);
 
         // Create a VBox pane for the circles
         VBox vBox = new VBox(20);
-        vBox.setStyle("-fx-border-color: black; -fx-background-color: grey");
-        vBox.setPadding(new Insets(20));
+        vBox.setStyle("-fx-border-color: black; -fx-background-color: white");
+        vBox.setPadding(new Insets(10));
         vBox.getChildren().addAll(circle[0], circle[1], circle[2]);
 
         // Create radio buttons
@@ -62,13 +62,12 @@ public class TrafficLightRadioButtonDemo extends Application{
 
         // Create a HBox for the radio buttons
         HBox hBox = new HBox(10);
-        hBox.setStyle("-fx-background-color: grey");
         hBox.setPadding(new Insets(20));
         hBox.getChildren().addAll(rbRed, rbYellow, rbGreen);
 
         // Create a pane for the vBox
         GridPane pane1 = new GridPane();
-        pane1.setPrefHeight(300);
+        pane1.setPrefHeight(200);
         pane1.setPrefWidth(100);
         pane1.setAlignment(Pos.CENTER);
         pane1.getChildren().addAll(vBox,hBox);
@@ -86,7 +85,7 @@ public class TrafficLightRadioButtonDemo extends Application{
         pane.setCenter(pane2);
 
         // Create the scene for the panes
-        Scene scene = new Scene(pane, 250, 450);
+        Scene scene = new Scene(pane, 200, 275);
         primaryStage.setTitle("Traffic Light");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
