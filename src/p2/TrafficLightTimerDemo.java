c117a17beaee2ed6a5c0a86b2df5d3ffd56bc1b9
Mayra Mendez <mmendez9@dtcc.edu>
2016-04-13 20:30:11 -0700

    More demos added

--- src/p2/TrafficLightTimerDemo.java
+++ src/p2/TrafficLightTimerDemo.java
@@ -23,21 +23,25 @@ import javafx.util.Duration;
 
 
 public class TrafficLightTimerDemo extends Application{
+    // Initialize three circles in the array
+    private Circle[] circle = new Circle[3];
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Initialize three circles in the array
-        Circle[] circle = new Circle[3];
 
+        // Circle for the red light
         circle[0] = new Circle();
         circle[0].setRadius(50);
         circle[0].setStroke(Color.BLACK);
         circle[0].setFill(Color.RED);
 
+        // Circle for the Yellow light
         circle[1] = new Circle();
         circle[1].setRadius(50);
         circle[1].setStroke(Color.BLACK);
         circle[1].setFill(Color.YELLOW);
 
+        // Circle for the green light
         circle[2] = new Circle();
         circle[2].setRadius(50);
         circle[2].setStroke(Color.BLACK);
@@ -45,7 +49,7 @@ public class TrafficLightTimerDemo extends Application{
 
         // Create a VBox pane for the circles
         Pane vBox = new VBox(20);
-        vBox.setStyle("-fx-border-color: black; -fx-background-color: black");
+        vBox.setStyle("-fx-border-color: black; -fx-background-color: beige");
         vBox.setPadding(new Insets(20));
         vBox.getChildren().addAll(circle[0], circle[1], circle[2]);
 
@@ -64,49 +68,55 @@ public class TrafficLightTimerDemo extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        EventHandler<ActionEvent> GreenToYellow = e -> {
-            if (circle[2].getFill() == Color.GREEN) {
-                circle[2].setFill(Color.DARKGREEN.darker());
-                circle[1].setFill(Color.YELLOW);
-                circle[0].setFill(Color.DARKRED.darker());
-            } else {
-                circle[2].setFill(Color.GREEN);
-                circle[1].setFill(Color.YELLOW.darker().darker());
-            }
-        };
-        EventHandler<ActionEvent> YellowToRed = e -> {
-            if (circle[1].getFill() == Color.YELLOW) {
-                circle[1].setFill(Color.YELLOW.darker().darker());
-                circle[0].setFill(Color.RED);
-                circle[2].setFill(Color.DARKGREEN.darker());
-            } else {
-                circle[1].setFill(Color.YELLOW);
-                circle[0].setFill(Color.DARKRED.darker());
-            }
-        };
-
-        EventHandler<ActionEvent> RedToGreen = e -> {
-            if (circle[0].getFill() == Color.RED) {
-                circle[0].setFill(Color.DARKRED.darker());
-                circle[1].setFill(Color.YELLOW.darker().darker());
-                circle[2].setFill(Color.GREEN.brighter());
-            } else {
-                circle[0].setFill(Color.RED);
-                circle[2].setFill(Color.DARKGREEN.darker());
-            }
-        };
-
         // Create an animation
-        Timeline animation1 = new Timeline(new KeyFrame(Duration.seconds(2), GreenToYellow));
+        Timeline animation1 = new Timeline(new KeyFrame(Duration.seconds(1), GreenToYellow));
         animation1.setCycleCount(Timeline.INDEFINITE);
         animation1.play();
 
-        Timeline animation2 = new Timeline(new KeyFrame(Duration.seconds(2), YellowToRed));
-        animation2.setCycleCount(Timeline.INDEFINITE);
-        animation2.play();
-
-        Timeline animation3 = new Timeline(new KeyFrame(Duration.seconds(2), RedToGreen));
+        /*Timeline animation3 = new Timeline(new KeyFrame(Duration.seconds(2), RedToGreen));
         animation3.setCycleCount(Timeline.INDEFINITE);
         animation3.play();
+
+        Timeline animation2 = new Timeline(new KeyFrame(Duration.seconds(1), YellowToRed));
+        animation2.setCycleCount(Timeline.INDEFINITE);
+        animation2.play();*/
     }
+
+    // Event to change from green to yellow
+    private EventHandler<ActionEvent> GreenToYellow = e -> {
+        if (circle[2].getFill() == Color.GREEN) {
+            circle[2].setFill(Color.WHITE);
+            circle[1].setFill(Color.YELLOW);
+            circle[0].setFill(Color.WHITE);
+        } else {
+            circle[2].setFill(Color.GREEN);
+            circle[1].setFill(Color.WHITE);
+            circle[0].setFill(Color.WHITE);
+        }
+    };
+
+    // Event to change from yellow to red
+    private EventHandler<ActionEvent> YellowToRed = e1 -> {
+        if (circle[1].getFill() == Color.YELLOW) {
+            circle[1].setFill(Color.WHITE);
+            circle[0].setFill(Color.RED);
+            circle[2].setFill(Color.WHITE);
+        } else {
+            circle[1].setFill(Color.YELLOW);
+            circle[0].setFill(Color.WHITE);
+        }
+    };
+
+    // Event to change from red to green
+    private EventHandler<ActionEvent> RedToGreen = e2 -> {
+        if (circle[0].getFill() == Color.RED) {
+            circle[0].setFill(Color.WHITE);
+            circle[1].setFill(Color.WHITE);
+            circle[2].setFill(Color.GREEN);
+        } else {
+            circle[0].setFill(Color.RED);
+            circle[2].setFill(Color.WHITE);
+        }
+    };
+
 }
